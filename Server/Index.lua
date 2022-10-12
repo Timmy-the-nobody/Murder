d74ff72087fe16d9e5afe2eb36c6d80acffa288a
Timmy <timmy.the.nobody@gmail.com>
2022-10-12 21:31:32 +0200

    chore: limit max players to code name count

--- Server/Index.lua
+++ Server/Index.lua
@@ -3,3 +3,8 @@ Package.Require( "modules/player.lua" )
 Package.Require( "modules/character.lua" )
 Package.Require( "modules/weapon.lua" )
 Package.Require( "modules/loot.lua" )
+
+if ( Server.GetMaxPlayers() > #GM.Cfg.CodeNames ) then
+    Server.SetMaxPlayers( #GM.Cfg.CodeNames, false )
+    Package.Warn( "Server max players is greater than the number of code names. Setting max players to " .. #GM.Cfg.CodeNames )
+end
